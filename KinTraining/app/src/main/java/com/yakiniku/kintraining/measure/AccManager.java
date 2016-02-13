771607bc4a840c72b75e9ddd646841d6daee6a60
sinlab <sin_lab@hotmail.co.jp>
2016-02-13 13:21:20 -0800

    update AccManager, delete dummy

--- KinTraining/app/src/main/java/com/yakiniku/kintraining/measure/AccManager.java
+++ KinTraining/app/src/main/java/com/yakiniku/kintraining/measure/AccManager.java
@@ -5,6 +5,8 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
+import com.yakiniku.kintraining.connect.ServerConnector;
+
 import java.util.List;
 
 /**
@@ -12,9 +14,21 @@ import java.util.List;
  */
 public class AccManager  implements SensorEventListener{
     private SensorManager manager;
+    private float ax,ay,az;
+    private float mx,my,mz;
+    private float vx,vy,vz;
+    private int counter;
+    private ServerConnector connector;
 
-    AccManager(SensorManager manager) {
+    public AccManager(SensorManager manager , ServerConnector connector) {
+        ax = ay = az = 0;
+        mx = my = mz = 0;
+        vx = vy = vz = 0;
+        counter = 0;
+
         this.manager = manager;
+        this.connector = connector;
+
         List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_ACCELEROMETER);
         if(sensors.size() > 0) {
             Sensor s = sensors.get(0);
@@ -30,13 +44,24 @@ public class AccManager  implements SensorEventListener{
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // TODO Auto-generated method stub
-        }
+    }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         // TODO Auto-generated method stub
         if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            Log.d("TAG","" + event.values[SensorManager.DATA_X] + " " + event.values[SensorManager.DATA_Y] + " " + event.values[SensorManager.DATA_Z]);
+            ax = event.values[SensorManager.DATA_X];
+            ay = event.values[SensorManager.DATA_Y];
+            az = event.values[SensorManager.DATA_Z];
+            vz = az - mz;
+
+            if(vz < -15.0 || 15.0 < vz) { // 速度が大きい時
+                Log.d("TAG", "count up : " + counter);
+                counter++;
             }
+            mx = ax;
+            my = ay;
+            mz = az;
         }
+    }
 }
