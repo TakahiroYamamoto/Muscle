d739678a292fbc7698a1743f14cef88c50c0cc33
lowlander256 <lowlander256@gmail.com>
2016-02-13 19:29:27 +0900

    IPアドレスを自分で調べるように修正

--- KinTrainingServer/server.js
+++ KinTrainingServer/server.js
@@ -11,6 +11,21 @@ function printCounter(device, counter, res) {
     res.end();
 }
 
+function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
+    }
+  }
+
+  return '0.0.0.0';
+}
+
 server.on('request', function(req, res) {
     var path = req.url.replace(/\//g, "");
     switch (path) {
@@ -57,5 +72,7 @@ server.on('request', function(req, res) {
     }
 });
 
-server.listen(1000, '10.10.55.237');
+var ip = getIPAddress();
+console.log("local IP address: " + ip);
+server.listen(1000, ip);
 console.log("server listening...");
